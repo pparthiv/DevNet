634966cd1c26f32af22b66ec8e4715e995292a64
POBOROJO <parijatbhatt1@gmail.com>
2024-05-08 14:23:22 +0530

    Change : main.jsx imported tailwind.css

--- client/web/dashboard2/src/main.jsx
+++ client/web/dashboard2/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { MaterialTailwindControllerProvider } from "@/context";
-import "../publi20c/css/tailwind.css";
+import "../public/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
