c0d1d45543bdda62bc4f71323e50d3480d4763d2
pparthiv <parthiv.iitg@gmail.com>
2024-05-28 03:24:52 +0530

    ADDED: Projects

--- client/web/dashboard2/src/routes.jsx
+++ client/web/dashboard2/src/routes.jsx
@@ -6,38 +6,43 @@ import {
     ServerStackIcon,
     RectangleStackIcon,
   } from "@heroicons/react/24/solid";
-  import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
-  import { SignIn, SignUp } from "@/pages/auth";
-  
-  const icon = {
-    className: "w-5 h-5 text-inherit",
-  };
-  
-  export const routes = [
-    {
-      layout: "dashboard",
-      pages: [
-        {
-          icon: <HomeIcon {...icon} />,
-          name: "dashboard",
-          path: "/home",
-          element: <Home />,
-        },
-        {
-          icon: <UserCircleIcon {...icon} />,
-          name: "profile",
-          path: "/profile",
-          element: <Profile />,
-        },
-        {
-          icon: <TableCellsIcon {...icon} />,
-          name: "projects",
-          path: "/tables",
-          element: <Tables />,
-        },
-      ],
-    },
-  ];
-  
-  export default routes;
-  
+import { Home, Profile, Tables, Notifications, ProjectDescription} from "@/pages/dashboard";
+import { SignIn, SignUp } from "@/pages/auth";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        icon: <UserCircleIcon {...icon} />,
+        name: "profile",
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        icon: <TableCellsIcon {...icon} />,
+        name: "projects",
+        path: "/tables",
+        element: <Tables />,
+      },
+      {
+        icon: <RectangleStackIcon {...icon} />,
+        name: "project-card",
+        path: "/project/:id",
+        element: <ProjectDescription />,
+      }
+    ],
+  },
+];
+
+export default routes;
